8b358c9f688258cf3046b170541b41ce73df3cd1
Ardintsev Sergey <archunter1369@gmail.com>
2023-01-31 22:34:05 +0300

    repo added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className='todo-app'>
       <TodoList />
+      <a
+        href='https://github.com/ardintsev-sergey/react-todolist'
+        target='_blank'
+        rel='noreferrer'
+      >
+        Repo
+      </a>
     </div>
   );
 }
